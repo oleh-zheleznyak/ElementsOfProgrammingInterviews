22900ba7b13dce54189ee2f08fa82f2be1c16758
Oleh Zheleznyak <oleh.zheleznyak@gmail.com>
2020-03-15 15:34:29 +0100

    added basic singly linked list impl

---
+++ src/Problems/Chapter08_LinkedLists/SinglyLinkedList.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Problems.Chapter08_LinkedLists
+{
+    public class SinglyLinkedList<T> : ICollection<T>
+        where T : IEquatable<T>
+    {
+        public SinglyLinkedList() { }
+
+        public SinglyLinkedList(IReadOnlyCollection<T> collection)
+        {
+            if (collection is null) throw new ArgumentNullException(nameof(collection));
+
+            Node current = null;
+            Node previous = null;
+
+            foreach (var element in collection)
+            {
+                current = new Node(element);
+
+                if (previous is null)
+                {
+                    Head = current;
+                }
+                else
+                {
+                    previous.Next = current;
+                }
+
+                previous = current;
+            }
+            Tail = current;
+            Count = collection.Count;
+        }
+
+        public Node? Head { get; set; }
+        public Node? Tail { get; set; }
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(T item)
+        {
+            var node = new Node() { Value = item };
+            if (Tail is null) // count == 0
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                Tail.Next = node;
+                Tail = node;
+            }
+            Count++;
+        }
+
+        public void Clear()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+
+        public bool Contains(T item) => ((IEnumerable<T>)this).Contains(item);
+
+        public void CopyTo(T[]? array, int arrayIndex)
+        {
+            if (array is null) throw new ArgumentNullException(nameof(array));
+            if (arrayIndex < 0 || arrayIndex >= array.Length) throw new ArgumentException(nameof(arrayIndex));
+            if (arrayIndex + Count > array.Length) throw new ArgumentException("Linked list contents won't fit into the array");
+
+            foreach (var item in this)
+                array[arrayIndex++] = item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = Head;
+
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            if (Count == 0) return false;
+
+            var comparer = EqualityComparer<T>.Default;
+
+            Node? current = Head;
+            Node? previous = null;
+
+            while (current != null)
+            {
+                if (comparer.Equals(item, current.Value))
+                {
+                    if (previous is null)
+                    {
+                        Head = current.Next;
+                    }
+                    else
+                    {
+                        previous.Next = current.Next;
+                    }
+
+                    Count--;
+                    return true;
+                }
+
+
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() =>
+            this.GetEnumerator();
+
+        public class Node
+        {
+            public Node() { }
+
+            public Node(T value)
+            {
+                Value = value;
+            }
+
+            public T Value { get; set; }
+            public Node Next { get; set; }
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter08_LinkedLists/SinglyLinkedListTests.cs
@@ -0,0 +1,104 @@
+﻿using Problems.Chapter08_LinkedLists;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Problems.Tests.Chapter08_LinkedLists
+{
+    public class SinglyLinkedListTests
+    {
+        [Fact]
+        public void Create()
+        {
+            var input = new[] { 1, 2, 3 };
+            var linkedList = new SinglyLinkedList<int>(input);
+
+            Assert.Equal(input, linkedList);
+        }
+
+
+        [Fact]
+        public void Remove_Middle()
+        {
+            var linkedList = new SinglyLinkedList<int>(new[] { 1, 2, 3 });
+
+            linkedList.Remove(2);
+
+            Assert.Equal(new[] { 1, 3 }, linkedList);
+        }
+
+        [Fact]
+        public void Remove_First()
+        {
+            var valueToRemove = 1;
+            var newHeadValue = 2;
+
+            var input = new[] { valueToRemove, newHeadValue, 3 };
+
+            var linkedList = new SinglyLinkedList<int>(input);
+            var head = linkedList.Head;
+
+            linkedList.Remove(valueToRemove);
+            var newHead = linkedList.Head;
+
+            Assert.NotSame(head, newHead);
+            Assert.Equal(newHeadValue, newHead.Value);
+            Assert.Same(newHead, head.Next);
+
+            Assert.Equal(new[] { 2, 3 }, linkedList);
+        }
+
+        [Fact]
+        public void Remove_Last()
+        {
+            var linkedList = new SinglyLinkedList<int>(new[] { 1, 2, 3 });
+
+            linkedList.Remove(3);
+
+            Assert.Equal(new[] { 1, 2 }, linkedList);
+        }
+
+        [Fact]
+        public void Add_AddsElementAtTheEnd()
+        {
+            var linkedList = new SinglyLinkedList<int>(new[] { 1, 2 });
+
+            linkedList.Add(3);
+
+            Assert.Equal(new[] { 1, 2, 3 }, linkedList);
+        }
+
+        [Fact]
+        public void Add_IcreasesCount()
+        {
+            var input = new[] { 1, 2 };
+
+            var linkedList = new SinglyLinkedList<int>(input);
+
+            linkedList.Add(3);
+
+            Assert.Equal(input.Length + 1, linkedList.Count);
+        }
+
+        [Fact]
+        public void Add_UpdatesTail()
+        {
+            var input = new[] { 1, 2 };
+
+            var linkedList = new SinglyLinkedList<int>(input);
+
+            var tail = linkedList.Tail;
+            Assert.Equal(tail.Value, input.Last());
+
+            var valueToAdd = 3;
+            linkedList.Add(valueToAdd);
+            var newTail = linkedList.Tail;
+
+            Assert.NotSame(tail, newTail);
+            Assert.Same(tail.Next, newTail);
+            Assert.Equal(valueToAdd, newTail.Value);
+        }
+    }
+}
