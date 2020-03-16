58eb0febaf2b27eb358108a35a0bf2368d373ae6
Oleh Zheleznyak <oleh.zheleznyak@gmail.com>
2020-03-16 14:15:52 +0100

    addded func to find overlap between linked lists

---
+++ src/Problems/Chapter08_LinkedLists/Overlap.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.Chapter08_LinkedLists
+{
+    public static partial class LinkedListExtensions
+    {
+        public static ISinglyLinkedListNode<T>? FindFirstOverlappingNode<T>(this ISinglyLinkedList<T> first, ISinglyLinkedList<T>? second)
+            where T : IEquatable<T>
+        {
+            if (second is null) throw new ArgumentNullException(nameof(second));
+
+            var firstLength = first.Count();
+            var secondLength = second.Count();
+            if (firstLength == 0 || secondLength == 0) return null;
+
+            var lengthDifference = Math.Abs(firstLength - secondLength);
+
+            var longest = firstLength > secondLength ? first : second;
+            var shortest = firstLength > secondLength ? second : first;
+
+            var nodeInLongest = longest.Head.AdvanceBy(lengthDifference);
+            var nodeInShortest = shortest.Head;
+
+            while (nodeInLongest != null && nodeInShortest != null && !ReferenceEquals(nodeInLongest, nodeInShortest))
+            {
+                nodeInLongest = nodeInLongest.Next;
+                nodeInShortest = nodeInShortest.Next;
+            }
+
+            return nodeInLongest;
+        }
+
+        private static ISinglyLinkedListNode<T>? AdvanceBy<T>(this ISinglyLinkedListNode<T> node, int steps)
+        {
+            for (int i = 0; i < steps; i++)
+                node = node?.Next;
+            return node;
+        }
+
+        private static int Count<T>(this ISinglyLinkedList<T> list)
+        {
+            var count = 0;
+            var node = list.Head;
+            while (node != null)
+            {
+                node = node.Next;
+                count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter08_LinkedLists/OverlapTests.cs
@@ -0,0 +1,53 @@
+﻿using Problems.Chapter08_LinkedLists;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Problems.Tests.Chapter08_LinkedLists
+{
+    public class OverlapTests
+    {
+        [Fact]
+        public void Overlap_Last()
+        {
+            var first = new SinglyLinkedList<int>(new[] { 2, 4, 100 });
+            var second = new SinglyLinkedList<int>(new[] { 1, 3 });
+
+            var expectedOverlap = first.Tail;
+            second.Tail.Next = expectedOverlap;
+
+            var actual = first.FindFirstOverlappingNode(second);
+
+            Assert.Equal(expectedOverlap, actual);
+        }
+
+        [Fact]
+        public void Overlap_Middle()
+        {
+            var first = new SinglyLinkedList<int>(new[] { 2, 4, 6 });
+            var second = new SinglyLinkedList<int>(new[] { 3 });
+
+            var expectedOverlap = first.Head.Next; // 4
+            second.Tail.Next = expectedOverlap;
+
+            var actual = first.FindFirstOverlappingNode(second);
+
+            Assert.Equal(expectedOverlap, actual);
+        }
+
+        [Fact]
+        public void Overlap_Beginning()
+        {
+            var first = new SinglyLinkedList<int>(new[] { 2, 3, 4 });
+            var second = new SinglyLinkedList<int>(new[] { 0, 1 });
+
+            var expectedOverlap = first.Head;
+            second.Tail.Next = expectedOverlap;
+
+            var actual = first.FindFirstOverlappingNode(second);
+
+            Assert.Equal(expectedOverlap, actual);
+        }
+    }
+}
