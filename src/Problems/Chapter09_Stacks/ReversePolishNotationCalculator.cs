4dc37d63a111563ab32966b3d02d65e336470ded
Oleh Zheleznyak <oleh.zheleznyak@gmail.com>
2020-03-17 16:43:31 +0100

    added reverse polish notation calculator with tests

---
+++ src/Problems/Chapter09_Stacks/ReversePolishNotationCalculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.Chapter09_Stacks
+{
+    public class ReversePolishNotationCalculator
+    {
+        private readonly char delimiter;
+
+        public ReversePolishNotationCalculator(char delimiter)
+        {
+            this.delimiter = delimiter;
+        }
+
+        public int Calculate(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input)) throw new ArgumentException(nameof(input));
+
+            var tokens = input.Split(delimiter);
+
+            return Calculate(tokens);
+        }
+
+        private int Calculate(string[] tokens)
+        {
+            var intermediateResults = new Stack<int>();
+
+            (int first, int second) TakeTwo() => (intermediateResults.Pop(), intermediateResults.Pop());
+
+            foreach (var token in tokens)
+            {
+                var result = ProcessToken(token, TakeTwo);
+                intermediateResults.Push(result);
+            }
+            return intermediateResults.Peek();
+        }
+
+        private int ProcessToken(string token, Func<(int, int)> operandSource)
+        {
+            if (int.TryParse(token, out var number))
+                return number;
+            else
+            {
+                var (first, second) = operandSource();
+                var result = ApplyOperand(token, second, first);
+                return result;
+            }
+        }
+
+        private int ApplyOperand(string operand, int first, int second) =>
+            operand switch
+            {
+                "+" => first + second,
+                "*" => first * second,
+                "-" => first - second,
+                "/" => first / second,
+                _ => throw new ArgumentException("Token not supported")
+            };
+    }
+}
---
+++ tests/Problems.Tests/Chapter09_Stacks/ReversePolishNotationCalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using Problems.Chapter09_Stacks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Problems.Tests.Chapter09_Stacks
+{
+    public class ReversePolishNotationCalculatorTests
+    {
+        ReversePolishNotationCalculator calc = new ReversePolishNotationCalculator(';');
+
+        [Theory]
+        [InlineData("1;1;+", 2)]
+        [InlineData("3;2;-", 1)]
+        [InlineData("2;3;*", 6)]
+        [InlineData("6;2;/", 3)]
+        [InlineData("1;2;+;5;*", 15)]
+        [InlineData("9;3;/;1;+", 4)]
+        public void Add(string input, int result)
+        {
+            Assert.Equal(result, calc.Calculate(input));
+        }
+    }
+}
