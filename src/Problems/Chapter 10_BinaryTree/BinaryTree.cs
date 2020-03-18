02a8a72ac5dd85fc5b358086c7b2d7acca642d8f
Oleh Zheleznyak <oleh.zheleznyak@gmail.com>
2020-03-18 16:35:44 +0100

    added a binary tree class and preorder traversal

---
+++ src/Problems/Chapter 10_BinaryTree/BinaryTree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.Chapter_10_BinaryTree
+{
+    public class BinaryTree<T>
+    {
+        public BinaryTree() { }
+        public BinaryTree(T value) { Value = value; }
+
+        public T Value { get; set; }
+        public BinaryTree<T> Left { get; set; }
+        public BinaryTree<T> Right { get; set; }
+
+        public bool IsLeaf => (Left is null) && (Right is null);
+
+        /// <summary>
+        /// visit the root, traverse left subtree,  traverse right subtree
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<T> Preorder()
+        {
+            var stack = new Stack<BinaryTree<T>>();
+            stack.Push(this);
+
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();
+                yield return node.Value;
+
+                if (node.Right != null) stack.Push(node.Right);
+                if (node.Left != null) stack.Push(node.Left);
+            }
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter10_BinaryTree/BinaryTreeTests.cs
@@ -0,0 +1,76 @@
+﻿using Problems.Chapter_10_BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Problems.Tests.Chapter10_BinaryTree
+{
+    public class BinaryTreeTests
+    {
+        BinaryTree<char> binaryTree;
+
+        public BinaryTreeTests()
+        {
+            binaryTree = new BinaryTree<char>()
+            {
+                 Value = 'A',
+                 Left = new BinaryTree<char>()
+                 {
+                     Value = 'B',
+                     Left = new BinaryTree<char>()
+                     {
+                         Value = 'C',
+                         Left = new BinaryTree<char>('D'),
+                         Right = new BinaryTree<char>('E')
+                     },
+                     Right = new BinaryTree<char>()
+                     {
+                         Value = 'F',
+                         Right = new BinaryTree<char>()
+                         {
+                             Value = 'G',
+                             Left = new BinaryTree<char>('H')
+                         }
+                     }
+                 },
+                 Right = new BinaryTree<char>()
+                 {
+                     Value = 'I',
+                     Left = new BinaryTree<char>()
+                     {
+                         Value = 'J',
+                         Right = new BinaryTree<char>()
+                         {
+                             Value = 'K',
+                             Left = new BinaryTree<char>()
+                             {
+                                 Value = 'L',
+                                 Right = new BinaryTree<char>('M')
+                             },
+                             Right = new BinaryTree<char>('N')
+                         }
+                     },
+                     Right = new BinaryTree<char>()
+                     {
+                         Value = 'O',
+                         Right = new BinaryTree<char>('P')
+                     }
+                 }
+            };
+        }
+
+        [Fact]
+        public void PreorderTest()
+        {
+            var preorder = binaryTree.Preorder().ToArray();
+
+            var actual = new string(preorder);
+
+            var expected = "ABCDEFGHIJKLMNOP";
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
