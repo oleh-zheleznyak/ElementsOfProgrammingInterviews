1ad26b33fa9caa86f1862c55440947dac5fd8483
oleh.zheleznyak <oleh.zheleznyak@spartez-software.com>
2021-08-19 21:37:42 +0200

    cache and xor for parity checks (used in internet checksum)

---
+++ src/Problems/Chapter05_PrimitiveTypes/ParityOfAWord.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Problems.Chapter05_PrimitiveTypes
+{
+    public class ParityOfAWord
+    {
+        public ulong CalculateParity_Linear(ulong word)
+        {
+            ulong parity = 0;
+            while (word > 0)
+            {
+                var bit = word & 1;
+                parity = parity ^ bit;
+                word >>= 1;
+            }
+
+            return parity;
+        }
+
+        public ulong CalculateParity_Efficient(ulong word)
+        {
+            ulong parity = 0;
+            while (word > 0)
+            {
+                parity = parity ^ 1;
+                word &= (word - 1);
+            }
+
+            return parity;
+        }
+
+        public ushort CalculateParity_Efficient(ushort word)
+        {
+            ushort parity = 0;
+            while (word > 0)
+            {
+                parity = (ushort)(parity ^ 1);
+                word &= (ushort)(word - 1);
+            }
+
+            return parity;
+        }
+
+        public ulong CalculateParity_BruteForce(ulong[] words)
+        {
+            if (words is null) throw new ArgumentNullException(nameof(words));
+            if (words.Length == 0) return 0;
+
+            ulong parity = 0;
+            foreach (var word in words)
+            {
+                parity = parity ^ CalculateParity_Linear(word);
+            }
+
+            return parity;
+        }
+
+        const byte wordSize = 16;
+        public ulong CalculateParity_Cached(ulong[] words)
+        {
+            if (words is null) throw new ArgumentNullException(nameof(words));
+            if (words.Length == 0) return 0;
+
+            ushort?[] cachedParity = new ushort?[ushort.MaxValue];
+
+            ulong parity = 0;
+            foreach (var word in words)
+            {
+                ushort wordParity = 0;
+                for (byte i = 0; i < 4; i++)
+                {
+                    ushort subWord = GetSubWord(word, i);
+
+                    if (!cachedParity[subWord].HasValue)
+                    {
+                        cachedParity[subWord] = CalculateParity_Efficient(subWord);
+                    }
+                    wordParity = (ushort)(wordParity ^ cachedParity[subWord]!.Value);
+                }
+                parity = parity ^ wordParity;
+            }
+
+            return parity;
+        }
+
+        private ushort GetSubWord(ulong word, byte part)
+        {
+            return (ushort)((word >> (part * wordSize)) & ushort.MaxValue);
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter05_PrimitiveTypes/ParityOfAWordTests.cs
@@ -0,0 +1,40 @@
+﻿using Problems.Chapter05_PrimitiveTypes;
+using Xunit;
+
+namespace Problems.Tests.Chapter05_PrimitiveTypes
+{
+    public class ParityOfAWordTests
+    {
+        ParityOfAWord parity = new ParityOfAWord();
+
+        [Theory]
+        [InlineData(0b_0000, 0)]
+        [InlineData(0b_0001, 1)]
+        [InlineData(0b_0011, 0)]
+        [InlineData(0b_0111, 1)]
+        public void ParityTest(ulong word, ulong expectedParity)
+        {
+            Assert.Equal(expectedParity, parity.CalculateParity_Linear(word));
+            Assert.Equal(expectedParity, parity.CalculateParity_Efficient(word));
+        }
+
+        [Theory]
+        [InlineData(new ulong[] { 0b_0000, 0b_0011 }, 0)]
+        [InlineData(new ulong[] { 0b_0001, 0b_0111 }, 0)]
+        [InlineData(new ulong[] { 0b_0011, 0b_0001 }, 1)]
+        public void ParityTestForArray(ulong[] word, ulong expectedParity)
+        {
+            Assert.Equal(expectedParity, parity.CalculateParity_BruteForce(word));
+        }
+
+        [Theory]
+        [InlineData(new ulong[] { 0b_0000, 0b_0011 }, 0)]
+        [InlineData(new ulong[] { 0b_0001, 0b_0111 }, 0)]
+        [InlineData(new ulong[] { 0b_0011, 0b_0001 }, 1)]
+        public void ParityTestCached(ulong[] word, ulong expectedParity)
+        {
+            Assert.Equal(expectedParity, parity.CalculateParity_Cached(word));
+        }
+
+    }
+}
