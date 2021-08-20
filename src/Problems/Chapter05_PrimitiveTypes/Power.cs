0357ade32381266da9a146376673245e1f49ae73
oleh.zheleznyak <oleh.zheleznyak@spartez-software.com>
2021-08-20 21:18:27 +0200

    5.7 efficient algorithm for computing power

---
+++ src/Problems/Chapter05_PrimitiveTypes/Power.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.Chapter05_PrimitiveTypes
+{
+    /// <summary>
+    /// 5.7 Compute x to power of y
+    /// </summary>
+    /// <remarks>
+    /// Regarding naming, it's best to put this in a Math class, Pow method, to use via static helper like Math.Pow
+    /// </remarks>
+    public class Power
+    {
+        public double Compute(double x, int y)
+        {
+            // TODO: Input sanitization: NaN, Infinities
+            if (x < 0 && y < 0) throw new ArgumentException($"Invalid argument combination, both {nameof(x)} and {nameof(y)} are negative");
+            if (y == 0) return 1;
+            if (y<0)
+            {
+                y = - y;
+                x = 1.0D / x;
+            }
+            var power = y;
+            var result = 1D;
+
+            while (power > 0)
+            {
+                if ((power & 1) == 1)
+                {
+                    result *= x;
+                }
+                x *= x;
+                power >>= 1;
+            }
+
+            return result; 
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter05_PrimitiveTypes/PowerTest.cs
@@ -0,0 +1,28 @@
+﻿using Problems.Chapter05_PrimitiveTypes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Problems.Tests.Chapter05_PrimitiveTypes
+{
+    public class PowerTest
+    {
+        private readonly Power power = new Power();
+
+        [Theory]
+        [InlineData(1, 0, 1)]
+        [InlineData(2, 0, 1)]
+        [InlineData(2, -1, 0.5)]
+        [InlineData(1, 1, 1)]
+        [InlineData(2, 2, 4)]
+        [InlineData(2, 3, 8)]
+        [InlineData(2, 4, 16)]
+        [InlineData(2, 8, 256)]
+        [InlineData(3, 3, 27)]
+        public void Test_WithPreComputed(double x, int y, double expected)
+        {
+            Assert.Equal(expected, power.Compute(x, y));
+        }
+    }
+}
