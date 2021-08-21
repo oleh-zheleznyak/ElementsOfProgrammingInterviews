865552355086fc282ca7aaa2ecfdbdf6509801a1
oleh.zheleznyak <oleh.zheleznyak@spartez-software.com>
2021-08-21 17:12:34 +0200

    dutch national flag problem

---
+++ src/Problems/Chapter06_Arrays/DutchNationalFlagPartition.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.Chapter06_Arrays
+{
+    /// <summary>
+    /// 6.1 Dutch National Flag Problem
+    /// </summary>
+    public class DutchNationalFlagPartition
+    {
+        public void Partition<T>(T[] data, int pivotIndex, IComparer<T>? comparerImpl = null)
+            where T : struct, IComparable<T>
+        {
+            if (data is null) throw new ArgumentNullException(nameof(data));
+            if (data.Length == 0) return;
+            if (pivotIndex < 0 || pivotIndex > data.Length - 1) throw new ArgumentOutOfRangeException(nameof(pivotIndex));
+
+            var comparer = comparerImpl ?? Comparer<T>.Default;
+
+            var pivot = data[pivotIndex];
+            var smaller = 0;
+            var greater = data.Length - 1;
+
+            Swap(data, smaller, pivotIndex);
+
+            var i = 1;
+            while (i <= greater)
+            {
+                var compared = comparer.Compare(data[i], pivot);
+                if (compared < 0) // data[i] < pivot
+                {
+                    Swap(data, smaller, i);
+                    smaller++;
+                    i++;
+                }
+                else if (compared > 0) // data[i] > pivot
+                {
+                    Swap(data, greater, i);
+                    greater--;
+                }
+                else // data[i] == pivot
+                {
+                    i++;
+                }
+            }
+
+        }
+
+        private void Swap<T>(T[] data, int i, int j)
+        {
+            var value = data[i];
+            data[i] = data[j];
+            data[j] = value;
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter06_Arrays/DutchNationalFlagPartitionTest.cs
@@ -0,0 +1,22 @@
+﻿using Problems.Chapter06_Arrays;
+using Xunit;
+
+namespace Problems.Tests.Chapter06_Arrays
+{
+    public class DutchNationalFlagPartitionTest
+    {
+        DutchNationalFlagPartition partition = new DutchNationalFlagPartition();
+
+        [Theory]
+        [InlineData(new int[] { 3, 2, 1 }, 1, new int[] { 1, 2, 3 })]
+        [InlineData(new int[] { 3, 1, 2, 3, 1 }, 2, new int[] { 1, 1, 2, 3, 3 })]
+        [InlineData(new int[] { 2,2,2 }, 1, new int[] { 2, 2, 2 })]
+        [InlineData(new int[] { 1 }, 0, new int[] { 1 })]
+        [InlineData(new int[] { }, 0, new int[] { })]
+        public void PartitionTest(int[] input, int index, int[] expected)
+        {
+            partition.Partition<int>(input, index);
+            Assert.Equal(expected, input);
+        }
+    }
+}
