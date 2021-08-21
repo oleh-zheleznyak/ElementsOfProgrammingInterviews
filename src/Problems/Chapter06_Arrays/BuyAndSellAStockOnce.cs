5fc11d54c141561267e2664e3cac00abbf8d8591
oleh.zheleznyak <oleh.zheleznyak@spartez-software.com>
2021-08-21 21:15:12 +0200

    buy and sell stock once - max profit

---
+++ src/Problems/Chapter06_Arrays/BuyAndSellAStockOnce.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.Chapter06_Arrays
+{
+    public class BuyAndSellAStockOnce
+    {
+        public double MaximumProfit(double[] stockPrices)
+        {
+            if (stockPrices is null) throw new ArgumentNullException(nameof(stockPrices));
+            if (stockPrices.Length <=1) return 0;
+
+            var maxProfit = 0D;
+            var minPrice = double.MaxValue;
+
+            for (int i = 0; i < stockPrices.Length; i++)
+            {
+                if (stockPrices[i] < minPrice) minPrice = stockPrices[i];
+
+                var profit = stockPrices[i] - minPrice;
+                if (profit > maxProfit) maxProfit = profit;
+            }
+
+            return maxProfit;
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter06_Arrays/BuyAndSellAStockOnceTest.cs
@@ -0,0 +1,24 @@
+﻿using Problems.Chapter06_Arrays;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Problems.Tests.Chapter06_Arrays
+{
+    public class BuyAndSellAStockOnceTest
+    {
+        BuyAndSellAStockOnce buyAndSellAStockOnce = new BuyAndSellAStockOnce();
+
+        [Theory]
+        [InlineData(new double[] { 310, 315, 275, 295, 260, 270, 290, 230, 255, 250 }, 30)]
+        [InlineData(new double[] { 5, 4, 3, 2, 1, 6 }, 5)]
+        [InlineData(new double[] { 4, 5, 6, 2 }, 2)]
+        [InlineData(new double[] { 1, 1, 1 }, 0)]
+        public void MaxProfitTest(double[] stockPrices, double expectedMaxProfit)
+        {
+            var actualProfit = buyAndSellAStockOnce.MaximumProfit(stockPrices);
+            Assert.Equal(expectedMaxProfit, actualProfit);
+        }
+    }
+}
