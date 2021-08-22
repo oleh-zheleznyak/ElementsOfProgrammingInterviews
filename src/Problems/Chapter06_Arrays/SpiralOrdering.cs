f58084f73c6a576c67639a10d8cd4d6afc10c87c
oleh.zheleznyak <oleh.zheleznyak@spartez-software.com>
2021-08-22 17:06:23 +0200

    spiral ordering of array

---
+++ src/Problems/Chapter06_Arrays/SpiralOrdering.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problems.Chapter06_Arrays
+{
+    /// <summary>
+    /// 6.17 Compute spiral ordering of a 2D array
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class SpiralOrdering<T>
+    {
+        public IEnumerable<T> EnumerateInSpiralOrder(T[,] array)
+        {
+            if (array is null) throw new ArgumentNullException(nameof(array));
+            if (array.Length == 0) yield break;
+
+            // TODO: sanitize input more dillignetly
+            var length = array.Length;
+
+            var minRow = -1;
+            var maxRow = array.GetLength(0); 
+            var minColumn = -1;
+            var maxColumn = array.GetLength(1); 
+
+            var row = 0;
+            var column = 0;
+            var counter = 0;
+            var direction = Direction.Right;
+
+            while (counter < length)
+            {
+                yield return array[row, column];
+                counter++;
+
+                switch (direction)
+                {
+                    case Direction.Right:
+                        column++;
+                        if (column == maxColumn)
+                        {
+                            column--;
+                            row++;
+                            minRow++;
+                            direction = Direction.Down;
+                        }
+                        break;
+                    case Direction.Down:
+                        row++;
+                        if (row == maxRow)
+                        {
+                            row--;
+                            column--;
+                            maxColumn--;
+                            direction = Direction.Left;
+                        }
+                        break;
+                    case Direction.Left:
+                        column--;
+                        if (column == minColumn)
+                        {
+                            column++;
+                            row--;
+                            maxRow--;
+                            direction = Direction.Up;
+                        }
+                        break;
+                    case Direction.Up:
+                        row--;
+                        if (row == minRow)
+                        {
+                            row++;
+                            column++;
+                            minColumn++;
+                            direction = Direction.Right;
+                        }
+                        break;
+                }
+            }
+            yield break;
+        }
+
+        private enum Direction : byte
+        {
+            Right = 1,
+            Down = 2,
+            Left = 3,
+            Up = 4,
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter06_Arrays/SpiralOrderingTest.cs
@@ -0,0 +1,33 @@
+﻿using Problems.Chapter06_Arrays;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Problems.Tests.Chapter06_Arrays
+{
+    public class SpiralOrderingTest
+    {
+        SpiralOrdering<int> spiralOrdering = new SpiralOrdering<int>();
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void EnumerateInSpiralOrderTest(int[,] array, int[] expectedOrder)
+        {
+            var actualOrder = spiralOrdering.EnumerateInSpiralOrder(array);
+            Assert.Equal(expectedOrder, actualOrder);
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[]
+            {
+                new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } },
+                new int[] { 1, 2, 3, 6, 9, 8, 7, 4, 5 }
+            };
+            yield return new object[]
+            {
+                new int[,] { {1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16} },
+                new int[] {1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10}
+            };
+        }
+    }
+}
