b8208742d400547003656ede9d8c48fdea3092ee
oleh.zheleznyak <oleh.zheleznyak@spartez-software.com>
2021-08-22 15:13:20 +0200

    sample offline data

---
+++ src/Problems/Chapter06_Arrays/SampleOfflineData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Problems.Chapter06_Arrays
+{
+    /// <summary>
+    /// 6.11 Sample offline data
+    /// </summary>
+    public class SampleOfflineData<T>
+    {
+        /// <summary>
+        /// Selects a random sample from input array. Every subset is equally likely
+        /// Mutates input array in place, to keep algorithm O(1) in terms of memory
+        /// </summary>
+        /// <param name="elements">collection of elements to select a sample from. Know to be unique</param>
+        /// <param name="sampleSize">size of the sample</param>
+        public void Sample(T[] elements, int sampleSize)
+        {
+            if (elements is null) throw new ArgumentNullException(nameof(elements));
+            if (sampleSize < 0 || sampleSize > elements.Length) throw new ArgumentException(nameof(sampleSize));
+            if (sampleSize == 0) return;
+
+            var random = new Random();
+            for (int i = 0; i < sampleSize; i++)
+            {
+                var index = random.Next(i, elements.Length - i);
+                elements.Swap(i, index);
+            }
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter06_Arrays/SampleOfflineDataTest.cs
@@ -0,0 +1,22 @@
+﻿using Problems.Chapter06_Arrays;
+using System.Linq;
+using Xunit;
+
+namespace Problems.Tests.Chapter06_Arrays
+{
+    public class SampleOfflineDataTest
+    {
+        SampleOfflineData<int> sampleOfflineData = new SampleOfflineData<int>();
+
+        [Theory]
+        [InlineData(new int[] { 1, 2 }, 0, new int[] { })]
+        [InlineData(new int[] { 1, 2 }, 2, new int[] { 1, 2 })]
+        [InlineData(new int[] { 1, 1, 1 }, 1, new int[] { 1 })]
+        public void SampleTest(int[] data, int sampleSize, int[] expected)
+        {
+            sampleOfflineData.Sample(data, sampleSize);
+            var actual = data.Take(sampleSize).ToArray();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
