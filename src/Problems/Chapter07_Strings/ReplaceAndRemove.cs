185aac16fc2bddf4667d78128a0affabd8655748
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2021-08-26 21:20:17 +0200

    Replace and remove

---
+++ src/Problems/Chapter07_Strings/ReplaceAndRemove.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Chapter07_Strings
+{
+    /// <summary>
+    /// 7.2 Replace and remove
+    /// </summary>
+    /// <remarks>
+    /// We can assume there is enough space in the array to hold the final result
+    /// </remarks>
+    public class ReplaceAndRemove
+    {
+        public char[] Process_WithLinearMemory(char[] array, int numberOfElementsToProcess)
+        {
+            if (array is null) throw new ArgumentNullException(nameof(array));
+            if (numberOfElementsToProcess < 0 || numberOfElementsToProcess > array.Length) throw new ArgumentException(nameof(numberOfElementsToProcess));
+
+            if (numberOfElementsToProcess == 0) return Array.Empty<char>();
+
+            var (count_a, count_b) = CountOccurrences(array, numberOfElementsToProcess);
+            if (count_a == 0 && count_b == 0) return array;
+
+            var finalLength = numberOfElementsToProcess + count_a - count_b;
+
+            var newArray = new char[finalLength];
+            var j = 0;
+            for (int i = 0; i < numberOfElementsToProcess; i++)
+            {
+                if (array[i] == 'a')
+                {
+                    newArray[j] = 'd';
+                    j++;
+                    newArray[j] = 'd';
+                    j++;
+
+                }
+                else if (array[i] == 'b')
+                {
+                    // do nothing
+                }
+                else
+                {
+                    newArray[j] = array[i];
+                    j++;
+                }
+            }
+            return newArray;
+        }
+
+        public char[] Process_WithConstantMemory(char[] array, int numberOfElementsToProcess)
+        {
+            if (array is null) throw new ArgumentNullException(nameof(array));
+            if (numberOfElementsToProcess < 0 || numberOfElementsToProcess > array.Length) throw new ArgumentException(nameof(numberOfElementsToProcess));
+
+            if (numberOfElementsToProcess == 0) return Array.Empty<char>();
+
+            var (count_a, count_b) = CountOccurrences(array, numberOfElementsToProcess);
+            if (count_a == 0 && count_b == 0) return array;
+
+            if (count_b > 0) HandleRemovals(array, numberOfElementsToProcess);
+
+            if (count_a > 0) HandleAdditions(array, numberOfElementsToProcess - count_b, count_a);
+
+            return array;
+        }
+
+        private void HandleAdditions(char[] array, int numberOfElementsAfterRemovals, int count_a)
+        {
+            // Note: at this point the array is ONLY going to grow
+            var tail = numberOfElementsAfterRemovals + count_a - 1;
+            for (int i = numberOfElementsAfterRemovals - 1; i >= 0; i--)
+            {
+                if (array[i] == 'a')
+                {
+                    array[i] = 'd';
+                    array[tail] = 'd';
+                    tail--;
+                }
+                array[tail] = array[i];
+                tail--;
+            }
+        }
+
+        // TODO: how to handle cases when array becomes shorter? {b}->{}
+        private static void HandleRemovals(char[] array, int numberOfElementsToProcess)
+        {
+            var shift = 0;
+            for (int i = 0; i < numberOfElementsToProcess; i++)
+            {
+                if (array[i] == 'b')
+                {
+                    shift++;
+                }
+                if (shift > 0)
+                {
+                    array[i] = i + shift < array.Length ? array[i + shift] : char.MinValue; // in C# we could use Nullable<char>
+                }
+            }
+        }
+
+        private static (int, int) CountOccurrences(char[] array, int numberOfElementsToProcess)
+        {
+            var (count_a, count_b) = (0, 0);
+            for (int i = 0; i < numberOfElementsToProcess; i++)
+            {
+                if (array[i] == 'a') count_a++;
+                if (array[i] == 'b') count_b++;
+            }
+            return (count_a, count_b);
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter07_Strings/ReplaceAndRemoveTests.cs
@@ -0,0 +1,92 @@
+﻿using Problems.Chapter07_Strings;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Problems.Tests.Chapter07_Strings
+{
+    public class ReplaceAndRemoveTests
+    {
+        readonly ReplaceAndRemove replacer = new();
+
+        [Theory]
+        [MemberData(nameof(TestData_ForLinearMemory))]
+        public void Process_WithLinearMemoryTest(char[] inputArray, int elementsToProcess, char[] expectedResult)
+        {
+            var actual = replacer.Process_WithLinearMemory(inputArray, elementsToProcess);
+            Assert.Equal(expectedResult, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData_ForConstantMemory))]
+        public void Process_WithConstantMemoryTest(char[] inputArray, int elementsToProcess, char[] expectedResult)
+        {
+            var actual = replacer.Process_WithConstantMemory(inputArray, elementsToProcess);
+            Assert.Equal(expectedResult, actual);
+        }
+
+        public static IEnumerable<object[]> TestData_ForConstantMemory() => TestData().Union(new object[][] { singleBChar_OneToProcess });
+
+        public static IEnumerable<object[]> TestData_ForLinearMemory() => TestData().Union(new object[][] { singleBChar_OneToProcessLinear });
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return emptyArray_ZeroToProcess;
+            yield return nonEmptyArray_ZeroToProcess;
+            yield return singleA_OneToProcess;
+            yield return arrayWithNoControlChars_EntirelyToProcess;
+            yield return mixedCharsOfAlltypes_AllToProcess;
+        }
+
+        private static object[] mixedCharsOfAlltypes_AllToProcess = new object[]
+        {
+            new char[] { 'a', 'c', 'b', 'a', char.MinValue },
+            4,
+            new char[] { 'd', 'd', 'c', 'd', 'd' }
+        };
+
+
+        private static object[] singleBChar_OneToProcess = new object[]
+        {
+            new char[] { 'b' },
+            1,
+            new char[] { char.MinValue }
+        };
+
+
+        private static object[] singleBChar_OneToProcessLinear = new object[]
+        {
+            new char[] { 'b' },
+            1,
+            new char[] { }
+        };
+
+        private static object[] arrayWithNoControlChars_EntirelyToProcess = new object[]
+        {
+            new char[] { 'f','e','g' },
+            3,
+            new char[] { 'f', 'e', 'g' }
+        };
+
+        private static object[] singleA_OneToProcess = new object[]
+        {
+            new char[] { 'a', char.MinValue },
+            1,
+            new char[] { 'd','d' }
+        };
+
+        private static object[] emptyArray_ZeroToProcess = new object[]
+        {
+            new char[] { },
+            0,
+            new char[] { }
+        };
+
+        private static object[] nonEmptyArray_ZeroToProcess = new object[]
+        {
+            new char[] { 'z' },
+            0,
+            new char[] { }
+        };
+    }
+}
