12ccb9a4b4229165239f101800a7b581ad328b88
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2021-08-29 21:36:32 +0200

    find cyckle start in singly linked list - with O(N) space :-(

---
+++ src/Problems/Chapter08_LinkedLists/Cyklicity2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problems.Chapter08_LinkedLists
+{
+    public class Cyklicity2
+    {
+        public Node<T>? FindCycleStart<T>(Node<T> head)
+        {
+            if (head == null) throw new ArgumentNullException(nameof(head));
+            if (head.Next == null) return null;
+
+            var slow = head;
+            var fast = head;
+            var hasCyckle = false;
+
+            while (fast != null && !hasCyckle)
+            {
+                slow = slow.Next;
+                fast = fast.Next?.Next;
+
+                if (slow == fast)
+                    hasCyckle = true;
+            }
+
+            if (!hasCyckle) return null;
+
+            var nodesInCycle = new HashSet<Node<T>>();
+            do
+            {
+                nodesInCycle.Add(slow); // last step will try to add fast, and will return false
+                slow = slow.Next;
+
+            } while (slow != fast);
+
+            slow = head;
+            while (!nodesInCycle.Contains(slow))
+            {
+                slow = slow.Next;
+            }
+            return slow;
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter08_LinkedLists/Cyklicity2Tests.cs
@@ -0,0 +1,42 @@
+﻿using Problems.Chapter08_LinkedLists;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Problems.Tests.Chapter08_LinkedLists
+{
+    public class Cyklicity2Tests
+    {
+        Cyklicity2 cyklicity = new Cyklicity2();
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void FindCycleStartTest(Node<int> list, Node<int> expectedCycleStart)
+        {
+            var actualCycleStart = cyklicity.FindCycleStart(list);
+            Assert.Same(expectedCycleStart, actualCycleStart);
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return TwoElementCycle();
+        }
+
+        private static object[] TwoElementCycle()
+        {
+            var first = new Node<int>(1);
+            var second = new Node<int>(2);
+            first.Next = second;
+            second.Next = first;
+
+            return new object[]
+            {
+                first,
+                first
+            };
+        }
+    }
+}
