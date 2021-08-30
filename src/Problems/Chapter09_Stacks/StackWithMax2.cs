29d0105c59aa43dfdb2abbb4e90e49e8d6ffefc0
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2021-08-30 21:24:25 +0200

    stack with max, O(n) additional storage

---
+++ src/Problems/Chapter09_Stacks/StackWithMax2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Chapter09_Stacks
+{
+    public class StackWithMax2<T>
+        where T : IComparable<T>
+    {
+        private const int InitialStorageSize = 1;
+
+        public StackWithMax2(IComparer<T> comparer) => this.comparer = comparer;
+
+        public StackWithMax2() : this(Comparer<T>.Default) { }
+
+        private readonly IComparer<T> comparer;
+        private ValueTuple<T, T>[] storage = new ValueTuple<T, T>[InitialStorageSize];
+        private int elementCount = 0;
+
+        private void EnsureEnoughStorage()
+        {
+            if (elementCount == storage.Length)
+            {
+                var newStorageSize = GetNewStorageSize(storage.Length);
+                var newStorage = new ValueTuple<T, T>[newStorageSize];
+                Array.Copy(storage, newStorage, storage.Length);
+                storage = newStorage;
+            }
+        }
+
+        private int GetNewStorageSize(int currentStorageSize) => currentStorageSize * 2;
+
+        public void Push(T value)
+        {
+            EnsureEnoughStorage();
+            T newMax;
+            if (elementCount == 0) newMax = value;
+            else
+            {
+                var (_, max) = storage[elementCount - 1];
+                newMax = comparer.Compare(value, max) < 0 ? max : value;
+            }
+            storage[elementCount++] = (value, newMax);
+        }
+
+        public T Peek()
+        {
+            EnsureNotEmpty();
+            return storage[elementCount - 1].Item1;
+        }
+
+        private void EnsureNotEmpty()
+        {
+            if (elementCount == 0) throw new InvalidOperationException("Stack is empty");
+        }
+
+        public T Pop()
+        {
+            EnsureNotEmpty();
+            return storage[--elementCount].Item1;
+
+        }
+
+        public T Max()
+        {
+            EnsureNotEmpty();
+            return storage[elementCount - 1].Item2;
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter09_Stacks/StackWithMax2Tests.cs
@@ -0,0 +1,65 @@
+﻿using Problems.Chapter09_Stacks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Problems.Tests.Chapter09_Stacks
+{
+    public class StackWithMax2Tests
+    {
+        [Fact]
+        public void Peek_ShouldNotChangeMax()
+        {
+            var stack = new StackWithMax2<int>();
+            stack.Push(5);
+
+            Assert.Equal(5, stack.Peek());
+            Assert.Equal(5, stack.Max());
+        }
+
+        [Fact]
+        public void Pop_ShoulReflectMaxChange()
+        {
+            var stack = new StackWithMax2<int>();
+            stack.Push(1);
+            Assert.Equal(1, stack.Max());
+            stack.Push(2);
+            Assert.Equal(2, stack.Max());
+
+            stack.Pop();
+            Assert.Equal(1, stack.Max());
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void MaxTest(int expectedMax, StackWithMax2<int> stackWithMax2)
+        {
+            Assert.Equal(expectedMax, stackWithMax2.Max());
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return IncreasingOrder();
+            yield return DecreasingOrder();
+            yield return SawTooth();
+        }
+
+        private static object[] IncreasingOrder() => new object[] { 5, PutArrayInStack(1, 2, 3, 4, 5) };
+        private static object[] DecreasingOrder() => new object[] { 5, PutArrayInStack(5, 4, 3, 2, 1) };
+        private static object[] SawTooth() => new object[] { 3, PutArrayInStack(1, 3, 1, 3, 1) };
+
+        private static StackWithMax2<T> PutArrayInStack<T>(params T[] data)
+            where T : IComparable<T>
+        {
+            var stackWithMax = new StackWithMax2<T>();
+            for (int i = 0; i < data.Length; i++)
+            {
+                stackWithMax.Push(data[i]);
+            }
+            return stackWithMax;
+        }
+    }
+}
