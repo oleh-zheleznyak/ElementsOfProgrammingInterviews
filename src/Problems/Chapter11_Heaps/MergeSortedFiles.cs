afbf14828af5bc010bbc18dd9f270fe6cd3a097c
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2021-09-04 21:30:20 +0200

    merge sorted files

---
+++ src/Problems/Chapter11_Heaps/MergeSortedFiles.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Chapter11_Heaps
+{
+    public class MergeSortedFiles<T>
+        where T : IComparable<T>
+    {
+        /// <remarks>
+        /// This is as required in the task, but not at all C#-ish, signature should use IEnumerable or IReadonlyCollection
+        /// </remarks>
+        public IEnumerable<T> MergeSortedSequences(T[][] sequences)
+        {
+            if (sequences == null) throw new ArgumentNullException(nameof(sequences));
+            if (sequences.Length == 0) return Array.Empty<T>();
+            if (sequences.Length == 1) return sequences[0];
+
+            var maxHeap = new MaxHeap<T>(sequences[0]);
+
+            for (int i = 1; i < sequences.Length; i++)
+            {
+                for (int j = 0; j < sequences[i].Length; j++)
+                {
+                    maxHeap.Push(sequences[i][j]);
+                }
+            }
+
+            return maxHeap.Sort();
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter11_Heaps/MergeSortedFilesTests.cs
@@ -0,0 +1,32 @@
+﻿using Problems.Chapter11_Heaps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Problems.Tests.Chapter11_Heaps
+{
+    public class MergeSortedFilesTests
+    {
+        private MergeSortedFiles<int> mergeSortedFiles = new();
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void MergeTest(int[][] input, int[] expectedOutput)
+        {
+            var actual = mergeSortedFiles.MergeSortedSequences(input).ToArray();
+            Assert.Equal(expectedOutput, actual);
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[]
+            {
+                new int[][] { new int[] { 1, 3 }, new int[] { 2, 4 } },
+                new int[] { 4,3,2,1 }
+            };
+        }
+    }
+}
