abf5dabfc2ce1ff87cad416053ae7727535e0e25
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2021-09-04 16:32:04 +0200

    max heap

---
+++ src/Problems/Chapter11_Heaps/MaxHeap.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Problems.Chapter11_Heaps
+{
+    public class MaxHeap<T>
+        where T : notnull, IComparable<T>
+    {
+        private T[] storage;
+        private int elementCount;
+        private IComparer<T> comparer = Comparer<T>.Default;
+
+        public MaxHeap()
+        {
+            storage = new T[1];
+            elementCount = 0;
+        }
+
+        public MaxHeap(T[] array)
+        {
+            if (array == null) throw new ArgumentNullException(nameof(array));
+
+            storage = new T[array.Length];
+            Array.Copy(array, storage, array.Length);
+            elementCount = array.Length;
+
+            var nonLeaf = array.Length / 2 - 1;
+            for (int i = nonLeaf; i >= 0; i--)
+                MaxHeapify(i);
+        }
+
+        public void Push(T value)
+        {
+            storage[elementCount] = value;
+            storage.Swap(0, elementCount++);
+            MaxHeapify(0);
+        }
+        public T Peek()
+        {
+            EnsureNotEmpty();
+            return storage[0];
+        }
+
+        public T Pop()
+        {
+            EnsureNotEmpty();
+            var max = storage[0];
+            storage[0] = storage[--elementCount];
+            MaxHeapify(0);
+
+            return max;
+        }
+
+        private void MaxHeapify(int index)
+        {
+            var left = MaxHeap<T>.LeftChild(index);
+            var right = MaxHeap<T>.RightChild(index);
+            var largest = index;
+
+            if (left < elementCount && Compare(index, left) < 0) largest = left;
+            if (right < elementCount && Compare(largest, right) < 0) largest = right;
+
+            if (largest != index)
+            {
+                storage.Swap(largest,index);
+                MaxHeapify(largest);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private int Compare(int i, int j) => comparer.Compare(storage[i], storage[j]);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int LeftChild(int i) => i << 1 | 1;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int RightChild(int i) => 2 * i + 2; // TODO: how to optimize?
+
+        public int Count => elementCount;
+
+        private void EnsureNotEmpty()
+        {
+            if (storage.Length == 0) throw new InvalidOperationException("Heap is empty");
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter11_Heaps/MaxHeapTests.cs
@@ -0,0 +1,33 @@
+﻿using Problems.Chapter11_Heaps;
+using Xunit;
+
+namespace Problems.Tests.Chapter11_Heaps
+{
+    public class MaxHeapTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2 }, 2)]
+        [InlineData(new int[] { 1, 3, 2 }, 3)]
+        [InlineData(new int[] { 1, 3, 4, 2 }, 4)]
+        [InlineData(new int[] { 1, 3, 2, 5, 4 }, 5)]
+        public void CreateAndPeekMaxTests(int[] inputArray, int expectedMax)
+        {
+            var maxHeap = new MaxHeap<int>(inputArray);
+            var actualMax = maxHeap.Peek();
+            Assert.Equal(expectedMax, actualMax);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2 }, 1)]
+        [InlineData(new int[] { 1, 3, 2 }, 2)]
+        [InlineData(new int[] { 1, 3, 4, 2 }, 3)]
+        [InlineData(new int[] { 1, 3, 2, 5, 4 }, 4)]
+        public void CreateAndPopMaxTests(int[] inputArray, int expectedPoppedMax)
+        {
+            var maxHeap = new MaxHeap<int>(inputArray);
+            maxHeap.Pop();
+            var actualMax = maxHeap.Peek();
+            Assert.Equal(expectedPoppedMax, actualMax);
+        }
+    }
+}
