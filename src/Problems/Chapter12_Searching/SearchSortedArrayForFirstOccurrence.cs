1040ce09ce383532ae3fe1a811fb78cb407abbfd
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2021-09-06 21:04:56 +0200

    first occurrence in sorted array

---
+++ src/Problems/Chapter12_Searching/SearchSortedArrayForFirstOccurrence.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Chapter12_Searching
+{
+    public class SearchSortedArrayForFirstOccurrence<T>
+        where T : IComparable<T>
+    {
+        private readonly IComparer<T> comparer;
+
+        public SearchSortedArrayForFirstOccurrence(IComparer<T>? comparer = null)
+        {
+            this.comparer = comparer ?? Comparer<T>.Default;
+        }
+
+        public int FindFirstOccurrence(T[] sortedArray, T valueToFind)
+        {
+            if (sortedArray == null) throw new ArgumentNullException(nameof(sortedArray));
+
+            var index = Array.BinarySearch<T>(sortedArray, valueToFind, comparer);
+
+            var firstOccurrence = index - 1;
+            while (
+                firstOccurrence >= 0 &&
+                comparer.Compare(sortedArray[firstOccurrence], sortedArray[index]) == 0)
+            {
+                firstOccurrence--;
+            }
+            return firstOccurrence + 1;
+        }
+    }
+}
