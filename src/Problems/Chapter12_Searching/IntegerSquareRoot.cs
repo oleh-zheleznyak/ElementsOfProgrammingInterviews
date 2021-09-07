d0fcaeeeb71a70c75b7db6160daaef37c07fca47
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2021-09-07 21:29:36 +0200

    integer square root

---
+++ src/Problems/Chapter12_Searching/IntegerSquareRoot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Chapter12_Searching
+{
+    /// <summary>
+    /// 12.4 Compute the integer square root
+    /// </summary>
+    public class IntegerSquareRoot
+    {
+        public int Sqrt(int value)
+        {
+            if (value < 0) throw new ArgumentException("negative values not supported"); // TODO: use unsigned integer?
+            if (value == 0) return 0;
+            if (value == 1) return 1;
+
+            var hi = value / 2;
+            var lo = 1;
+            var mid = lo + (hi - lo) / 2;
+            while (lo <= hi)
+            {
+                mid = lo + (hi - lo) / 2;
+                var sq = mid * mid;
+                if (sq == value) return mid;
+                else if (sq > value) hi = mid - 1;
+                else lo = mid + 1;
+            }
+            return lo * lo < value ? lo : hi;
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter12_Searching/IntegerSquareRootTests.cs
@@ -0,0 +1,38 @@
+﻿using Problems.Chapter12_Searching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Problems.Tests.Chapter12_Searching
+{
+    public class IntegerSquareRootTests
+    {
+        IntegerSquareRoot integerSquareRoot = new();
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 1)]
+        [InlineData(3, 1)]
+        [InlineData(4, 2)]
+        [InlineData(5, 2)]
+        [InlineData(6, 2)]
+        [InlineData(7, 2)]
+        [InlineData(8, 2)]
+        [InlineData(9, 3)]
+        [InlineData(10, 3)]
+        [InlineData(11, 3)]
+        [InlineData(12, 3)]
+        [InlineData(13, 3)]
+        [InlineData(14, 3)]
+        [InlineData(15, 3)]
+        [InlineData(16, 4)]
+        public void SqrtTests(int value, int expectedSqrt)
+        {
+            var actualSqrt = integerSquareRoot.Sqrt(value);
+            Assert.Equal(expectedSqrt, actualSqrt);
+        }
+    }
+}
