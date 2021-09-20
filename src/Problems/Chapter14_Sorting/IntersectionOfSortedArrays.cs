9a69db1f5ee02df13f3f7110403797a235180ee1
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2021-09-20 21:18:53 +0200

    compute intersection of two sorted arrays

---
+++ src/Problems/Chapter14_Sorting/IntersectionOfSortedArrays.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problems.Chapter14_Sorting
+{
+    /// <summary>
+    /// 14.1 Compute the intersection of two sorted arrays
+    /// </summary>
+    public class IntersectionOfSortedArrays<T>
+        where T : IComparable<T>
+    {
+        private readonly IComparer<T> comparer;
+
+        public IntersectionOfSortedArrays(IComparer<T>? comparer = null)
+        {
+            this.comparer = comparer ?? Comparer<T>.Default;
+        }
+
+        public IReadOnlyCollection<T> Intersection(T[] firstSortedArray, T[] secondSortedArray)
+        {
+            if (firstSortedArray is null) throw new ArgumentNullException(nameof(firstSortedArray));
+            if (secondSortedArray is null) throw new ArgumentNullException(nameof(secondSortedArray));
+            if (ReferenceEquals(firstSortedArray, secondSortedArray)) return firstSortedArray;
+            if (firstSortedArray.Length == 0 || secondSortedArray.Length == 0) return Array.Empty<T>();
+
+            return IntersectTwoDifferentNonEmptySortedArrays(firstSortedArray, secondSortedArray);
+        }
+
+        private IReadOnlyCollection<T> IntersectTwoDifferentNonEmptySortedArrays(T[] firstSortedArray, T[] secondSortedArray)
+        {
+            var first = 0;
+            var second = 0;
+            var intersection = new List<T>(); // TODO: make this more elegant, by implementing IEnumerable and avoiding helper collection
+
+            do
+            {
+                var comparison = comparer.Compare(firstSortedArray[first], secondSortedArray[second]);
+                if (comparison == 0)
+                {
+                    if (intersection.Count > 0)
+                    {
+                        var lastAdded = intersection[intersection.Count - 1];
+                        if (comparer.Compare(lastAdded, firstSortedArray[first]) != 0)
+                            intersection.Add(firstSortedArray[first]);
+                    }
+                    else intersection.Add(firstSortedArray[first]);
+
+                    first++;
+                    second++;
+                }
+                else if (comparison < 0) first++;
+                else second++;
+
+
+            } while (first < firstSortedArray.Length && second < secondSortedArray.Length);
+
+            return intersection;
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter14_Sorting/IntersectionOfSortedArraysTests.cs
@@ -0,0 +1,18 @@
+﻿using Problems.Chapter14_Sorting;
+using Xunit;
+
+namespace Problems.Tests.Chapter14_Sorting
+{
+    public class IntersectionOfSortedArraysTests
+    {
+        private readonly IntersectionOfSortedArrays<int> intersectionOfSortedArrays = new();
+
+        [Theory]
+        [InlineData(new int[] { 2, 3, 3, 5, 5, 6, 7, 7, 8, 12 }, new int[] { 5, 5, 6, 8, 8, 9, 10, 10 }, new int[] { 5, 6, 8 })]
+        public void IntersectTest(int[] firstSortedArray, int[] secondSortedArray, int[] expectedResult)
+        {
+            var actual = intersectionOfSortedArrays.Intersection(firstSortedArray, secondSortedArray);
+            Assert.Equal(expectedResult, actual);
+        }
+    }
+}
