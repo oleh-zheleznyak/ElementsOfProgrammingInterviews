020a8153947016a0cf99bb478fb53ed5cd9b276e
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2021-09-23 09:09:51 +0200

    test if a binary tree satisfies the bst property

---
+++ src/Problems/Chapter15_BinarySearchTrees/TestIfABinaryTreeSatisfiesBstProperty.cs
@@ -0,0 +1,43 @@
+﻿using Problems.Chapter_10_BinaryTree;
+using System;
+using System.Collections.Generic;
+
+namespace Problems.Chapter15_BinarySearchTrees
+{
+    public class TestIfABinaryTreeSatisfiesBstProperty<T>
+        where T : IComparable<T>
+    {
+        private readonly IComparer<T> comparer;
+
+        public TestIfABinaryTreeSatisfiesBstProperty(IComparer<T>? comparer = null)
+        {
+            this.comparer = comparer ?? Comparer<T>.Default;
+        }
+
+        public bool IsABst(BinaryTree<T> binaryTree)
+        {
+            if (binaryTree is null) throw new ArgumentNullException(nameof(binaryTree));
+
+            return NodeSatisfiesBstPropertyRecursive(binaryTree);
+        }
+
+
+        private bool NodeSatisfiesBstPropertyRecursive(BinaryTree<T> binaryTree)
+        {
+            var satisfiesBstAtThisLevel = NodeSatisfiesBstProperty(binaryTree);
+
+            if (!satisfiesBstAtThisLevel) return false;
+
+            var satisfiesBstRecursively =
+                satisfiesBstAtThisLevel &&
+                binaryTree.Left is null ? true : NodeSatisfiesBstPropertyRecursive(binaryTree.Left) &&
+                binaryTree.Right is null ? true : NodeSatisfiesBstPropertyRecursive(binaryTree.Right);
+
+            return satisfiesBstRecursively;
+        }
+
+        private bool NodeSatisfiesBstProperty(BinaryTree<T> node) =>
+            node.Left is null ? true : comparer.Compare(node.Left.Value, node.Value) <= 0 &&
+            node.Right is null ? true : comparer.Compare(node.Right.Value, node.Value) >= 0;
+    }
+}
---
+++ tests/Problems.Tests/Chapter15_BinarySearchTrees/TestIfABinaryTreeSatisfiesBstPropertyTest.cs
@@ -0,0 +1,82 @@
+﻿using Problems.Chapter_10_BinaryTree;
+using Problems.Chapter15_BinarySearchTrees;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Problems.Tests.Chapter15_BinarySearchTrees
+{
+    public class TestIfABinaryTreeSatisfiesBstPropertyTest
+    {
+        private readonly TestIfABinaryTreeSatisfiesBstProperty<int> testForBst = new();
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void IsABstTest(BinaryTree<int> binaryTree, bool expectedIsBinary)
+        {
+            var actual = testForBst.IsABst(binaryTree);
+            Assert.Equal(expectedIsBinary, actual);
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return PerfectTwoLevelBst();
+            yield return TrivialSingleNode();
+            yield return PerfectTwoLevelNotABst();
+            yield return PerfectTwoLevelAllEqual();
+            yield return ThreeLevelBst();
+        }
+
+        private static object[] PerfectTwoLevelBst() =>
+            new object[]
+            {
+                new BinaryTree<int>(2)
+                {
+                    Left = new BinaryTree<int>(1),
+                    Right = new BinaryTree<int>(3)
+                },
+                true
+            };
+
+        private static object[] TrivialSingleNode() =>
+            new object[]
+            {
+                        new BinaryTree<int>(42),
+                        true
+            };
+        private static object[] PerfectTwoLevelNotABst() =>
+            new object[]
+            {
+                new BinaryTree<int>(2)
+                {
+                    Left = new BinaryTree<int>(3),
+                    Right = new BinaryTree<int>(1)
+                },
+                false
+            };
+
+        private static object[] PerfectTwoLevelAllEqual() =>
+            new object[]
+            {
+                new BinaryTree<int>(1)
+                {
+                    Left = new BinaryTree<int>(1),
+                    Right = new BinaryTree<int>(1)
+                },
+                true
+            };
+        private static object[] ThreeLevelBst() =>
+            new object[]
+            {
+                new BinaryTree<int>(2)
+                {
+                    Left = new BinaryTree<int>(1),
+                    Right = new BinaryTree<int>(4)
+                    {
+                        Left = new BinaryTree<int>(3),
+                        Right = new BinaryTree<int>(5)
+                    }
+                },
+                true
+            };
+    }
+}
