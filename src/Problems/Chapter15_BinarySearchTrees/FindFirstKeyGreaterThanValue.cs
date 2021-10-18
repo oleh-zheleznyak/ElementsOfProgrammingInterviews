ab782709c927496262045bed2364bd303786b149
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2021-10-18 20:59:54 +0200

    corrected the algorithm, enabled broken test cases

--- src/Problems/Chapter15_BinarySearchTrees/FindFirstKeyGreaterThanValue.cs
+++ src/Problems/Chapter15_BinarySearchTrees/FindFirstKeyGreaterThanValue.cs
@@ -5,7 +5,7 @@ using Problems.Chapter_10_BinaryTree;
 namespace Problems.Chapter15_BinarySearchTrees
 {
     public class FindFirstKeyGreaterThanValue<T>
-        where T: IComparable<T>
+        where T : IComparable<T>
     {
         private readonly IComparer<T> comparer;
 
@@ -13,29 +13,33 @@ namespace Problems.Chapter15_BinarySearchTrees
         {
             this.comparer = comparer ?? Comparer<T>.Default;
         }
-        
+
         /// <summary>
         /// Finds the minimum key that is greater than the value provided, or null.
         /// The tree passed in the argument MUST be a BST
         /// </summary>
         /// <param name="binarySearchTree">binary search tree is ASSUMED</param>
-        /// <param name="value">value to look for</param>
+        /// <param name="valueToFind">value to look for</param>
         /// <returns>minimum value in the tree that is greater than value or null</returns>
-        public T? FindMinGreaterThan(BinaryTree<T> binarySearchTree, T value)
+        public T? FindMinGreaterThan(BinaryTree<T> binarySearchTree, T valueToFind)
         {
             if (binarySearchTree is null) throw new ArgumentNullException(nameof(binarySearchTree));
 
             var current = binarySearchTree;
-            T previousValue = default;
-            while (current!=null)
+            T? min = default;
+            while (current != null)
             {
-                previousValue = current.Value;
-                var comparison = comparer.Compare(current.Value, value);
+                var comparison = comparer.Compare(current.Value, valueToFind);
                 if (comparison <= 0) current = current.Right;
-                else current = current.Left;
+                else
+                {
+                    if (comparer.Compare(min, default) == 0) min = current.Value;
+                    else min = comparer.Compare(min, current.Value) < 0 ? min : current.Value;
+                    current = current.Left;
+                }
             }
 
-            return previousValue;
+            return min;
         }
     }
 }
--- tests/Problems.Tests/Chapter15_BinarySearchTrees/FindFirstKeyGreaterThanValueTests.cs
+++ tests/Problems.Tests/Chapter15_BinarySearchTrees/FindFirstKeyGreaterThanValueTests.cs
@@ -26,7 +26,7 @@ namespace Problems.Tests.Chapter15_BinarySearchTrees
             yield return new object[] { tree, 4, 5 };
             yield return new object[] { tree, 11, 13 };
             yield return new object[] { tree, 12, 13 };
-            // yield return new object[] { tree, 18, 19 }; - a good example that algorithm is not correct
+            yield return new object[] { tree, 18, 19 };
             yield return new object[] { tree, 40, 41 };
             yield return new object[] { tree, 47, 53 };
         }
