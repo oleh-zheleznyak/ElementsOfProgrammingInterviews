074e5b7bb7b49216ff8c4e8d0646afdbbaeb1758
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2021-10-19 19:48:08 +0200

    find the kth largest elements in a bst - changed from brute force inorder traversal to reverse inorder traversal

--- src/Problems/Chapter15_BinarySearchTrees/FindKLargestElementsInBST.cs
+++ src/Problems/Chapter15_BinarySearchTrees/FindKLargestElementsInBST.cs
@@ -15,28 +15,17 @@ namespace Problems.Chapter15_BinarySearchTrees
         public IEnumerable<T> FindLargestElements(BinaryTree<T> bst, ushort numberOfElementsToFind)
         {
             if (bst is null) throw new ArgumentNullException(nameof(bst));
-            if (numberOfElementsToFind == 0) yield break;
+            if (numberOfElementsToFind == 0) return Enumerable.Empty<T>();
 
-            var inorder = Inorder(bst).ToList();
-            if (numberOfElementsToFind > inorder.Count) 
-                throw new ArgumentException($"{nameof(numberOfElementsToFind)} is greater than the amount of elements in the collection {nameof(bst)}");
-
-            for (var i = 0; i < numberOfElementsToFind; i++)
-                yield return inorder[inorder.Count - 1 - i];
+            return ReverseInorderTraversal(bst).Take(numberOfElementsToFind);
         }
 
-        /// <remarks>
-        /// This particular implementation of inorder traversal does not return the count of elements,
-        /// as it is lazily evaluated
-        /// This traversal is LESS elegant then existing in BinaryTree.Inorder - specifically to show
-        /// that we cannot rely on any particular implementation
-        /// </remarks>
-        private IEnumerable<T> Inorder(BinaryTree<T>? bst)
+        private IEnumerable<T> ReverseInorderTraversal(BinaryTree<T>? bst)
         {
             if (bst is null) return Enumerable.Empty<T>();
-            return Inorder(bst.Left)
+            return ReverseInorderTraversal(bst.Right)
                 .Concat(Enumerable.Repeat(bst.Value, 1))
-                .Concat(Inorder(bst.Right));
+                .Concat(ReverseInorderTraversal(bst.Left));
         }
     }
 }
