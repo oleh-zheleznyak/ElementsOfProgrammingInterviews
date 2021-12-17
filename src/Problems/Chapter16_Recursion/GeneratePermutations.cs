519b0899c776c77dbd41dfb0e3b543339d8f1ca7
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2021-12-17 08:57:02 +0100

    Permutations of an array

---
+++ src/Problems/Chapter16_Recursion/GeneratePermutations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems.Chapter16_Recursion
+{
+    public class GeneratePermutations<T>
+    {
+        public IEnumerable<T[]> AllPermutationsOf(ISet<T> array)
+        {
+            if (array is null) throw new ArgumentNullException(nameof(array));
+            if (array.Count == 0) return Enumerable.Empty<T[]>();
+            if (array.Count == 1) return Enumerable.Repeat(array.ToArray(), 1);
+
+            var result = new List<T[]>();
+            AllPermutationsOf(array, new T[array.Count], 0, result);
+            return result;
+        }
+
+        private void AllPermutationsOf(ISet<T> set, T[] permutation, int size, ICollection<T[]> results)
+        {
+            if (set.Count == 0)
+            {
+                results.Add(permutation);
+                return;
+            }
+
+            foreach (var item in set)
+            {
+                permutation[size] = item;
+                var newSet = new HashSet<T>(set);
+                newSet.Remove(item);
+                AllPermutationsOf(newSet, permutation.ToArray(), size + 1, results);
+            }
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter16_Recursion/GeneratePermutationsTests.cs
@@ -0,0 +1,31 @@
+﻿using Problems.Chapter16_Recursion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Problems.Tests.Chapter16_Recursion
+{
+    public class GeneratePermutationsTests
+    {
+        GeneratePermutations<int> sut = new();
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void AllPermutationsOfTest(int[] input, IEnumerable<int[]> expected)
+        {
+            var set = new HashSet<int>(input);
+            var actual = sut.AllPermutationsOf(set);
+            Assert.Equal(expected, actual);
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { Array.Empty<int>(), Enumerable.Empty<int[]>() };
+            yield return new object[] { new int[] { 1 }, new[] { new int[] { 1 } } };
+            yield return new object[] { new int[] { 1, 2 }, new[] { new int[] { 1, 2 }, new int[] { 2, 1 } } };
+            yield return new object[] { new int[] { 1, 2, 3 }, new[] { new int[] { 1, 2, 3 }, new int[] { 1, 3, 2 }, new int[] { 2, 1, 3 }, new int[] { 2, 3, 1 }, new int[] { 3, 1, 2 }, new int[] { 3, 2, 1 } } };
+        }
+
+    }
+}
