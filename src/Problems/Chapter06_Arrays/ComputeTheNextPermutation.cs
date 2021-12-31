dcdaa7dba131fda918663414c31e2b66c6710d95
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2021-12-31 15:35:25 +0100

    rewrite incorrect algorithm for next permutation in dictionary ordering, add test cases

--- src/Problems/Chapter06_Arrays/ComputeTheNextPermutation.cs
+++ src/Problems/Chapter06_Arrays/ComputeTheNextPermutation.cs
@@ -10,21 +10,60 @@ namespace Problems.Chapter06_Arrays
             if (permutation is null) throw new ArgumentNullException(nameof(permutation));
             if (permutation.Length <= 1) return permutation;
 
-            var result = permutation.ToArray();
-            var max = permutation.Length - 1;
-            if (permutation[max] == max)
-            {
-                result.Swap(max, max - 1);
-                return result;
-            }
-            var index = max;
-            while (permutation[index] >= result[index] && index >= 1)
-            {
-                result.Swap(index, index - 1);
-                index--;
-            }
+            var endOfSequenceIndex = FindDecreasingSequenceEnd(permutation);
+            if (endOfSequenceIndex < 0) return Array.Empty<int>();
 
-            return result;
+            var minimumIndex = FindMinLargerThan(permutation[endOfSequenceIndex], permutation, endOfSequenceIndex + 1);
+            var nextPermutation = permutation.ToArray();
+
+            nextPermutation.Swap(endOfSequenceIndex, minimumIndex);
+
+            ReverseStartingAtIndex(endOfSequenceIndex + 1, nextPermutation);
+
+            return nextPermutation;
+        }
+
+        private void ReverseStartingAtIndex(int v, int[] nextPermutation)
+        {
+            var count = (nextPermutation.Length - v + 1) / 2;
+            for (int i = 0; i < count; i++)
+                nextPermutation.Swap(v + i, nextPermutation.Length - 1 - i);
+        }
+
+        private int FindDecreasingSequenceEnd(int[] permutation)
+        {
+            var prev = permutation[permutation.Length - 1];
+
+            for (int i = permutation.Length - 2; i >= 0; i--)
+            {
+                var current = permutation[i];
+
+                if (current < prev) return i;
+
+                prev = current;
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// here, such minimum always exists, otherwise the whole permutation is a monotonously decreasing seqence
+        /// </summary>
+        private int FindMinLargerThan(int value, int[] permutation, int startIndex)
+        {
+            var minIndex = startIndex;
+            var min = permutation[minIndex];
+
+            for (int i = startIndex; i < permutation.Length; i++)
+            {
+                var current = permutation[i];
+
+                if (current < min && current > value)
+                {
+                    min = current;
+                    minIndex = i;
+                }
+            }
+            return minIndex;
         }
     }
 }
--- tests/Problems.Tests/Chapter06_Arrays/ComputeTheNextPermutationTest.cs
+++ tests/Problems.Tests/Chapter06_Arrays/ComputeTheNextPermutationTest.cs
@@ -9,6 +9,11 @@ namespace Problems.Tests.Chapter06_Arrays
 
         [Theory]
         [InlineData(new int[] { 1, 0, 3, 2 }, new int[] { 1, 2, 0, 3 })]
+        [InlineData(new int[] { 3, 0, 1, 2 }, new int[] { 3, 0, 2, 1 })]
+        [InlineData(new int[] { 3, 0, 2, 1 }, new int[] { 3, 1, 0, 2 })]
+        [InlineData(new int[] { 3, 1, 0, 2 }, new int[] { 3, 1, 2, 0 })]
+        [InlineData(new int[] { 3, 2, 1, 0 }, new int[] { })]
+        [InlineData(new int[] { 6, 2, 1, 5, 4, 3, 0 }, new int[] { 6, 2, 3, 0, 1, 4, 5 })]
         public void NextPermutationUnderDictionaryOrderingTest(int[] permutation, int[] nextPermutation)
         {
             var actual = sut.NextPermutationUnderDictionaryOrdering(permutation);
