433bca4393896ac44cf8c93bb78b73e9b02852e6
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2022-01-29 15:08:58 +0100

    generate all strings with given number of matched parens

---
+++ src/Problems/Chapter16_Recursion/GenerateStringsOfMatchedParens.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace Problems.Chapter16_Recursion
+{
+    public class GenerateStringsOfMatchedParens
+    {
+        private const string onePair = "()";
+        public IReadOnlyCollection<string> AllStringsWithMachedNumberOfParens(uint numberOfMatchedParensPairs)
+        {
+            if (numberOfMatchedParensPairs == 0) return new[] { string.Empty };
+            if (numberOfMatchedParensPairs == 1) return new[] { onePair };
+
+            var result = GenerateStringsWithParens(numberOfMatchedParensPairs);
+
+            return result;
+        }
+
+        private IReadOnlyCollection<string> GenerateStringsWithParens(uint numberOfMatchedParensPairs)
+        {
+            if (numberOfMatchedParensPairs == 1) return new[] { onePair };
+
+            var resultForSmallerPair = GenerateStringsWithParens(numberOfMatchedParensPairs - 1U);
+
+            var result = new List<string>(resultForSmallerPair.Count * 3); // possible overflow
+
+            foreach (var item in resultForSmallerPair)
+            {
+                result.Add(Enclose(item));
+                result.Add(item + onePair);
+                if (onePair != item) result.Add(onePair + item);
+            }
+
+            return result;
+        }
+
+        private string Enclose(string item) => $"({item})";
+    }
+}
---
+++ tests/Problems.Tests/Chapter16_Recursion/GenerateStringsOfMatchedParensTests.cs
@@ -0,0 +1,26 @@
+﻿using Problems.Chapter16_Recursion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Problems.Tests.Chapter16_Recursion
+{
+    public class GenerateStringsOfMatchedParensTests
+    {
+        GenerateStringsOfMatchedParens sut = new();
+
+        [Theory]
+        [InlineData(0, new[] { "" })]
+        [InlineData(1, new[] { "()" })]
+        [InlineData(2, new[] { "()()", "(())" })]
+        [InlineData(3, new[] { "()()()", "()(())", "(())()", "((()))", "(()())" })]
+        public void AllStringsWithMachedNumberOfParensTest(uint numberOfParensPairs, string[] expectedResult)
+        {
+            var actual = sut.AllStringsWithMachedNumberOfParens(numberOfParensPairs);
+            TestUtils.AssertEquivalent(expectedResult, actual);
+        }
+    }
+}
