67700c05dc0c9d14b8dcaaac2c3a73b080cd2516
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2022-01-30 16:10:41 +0100

    compute all palindromic decompositions of a string

---
+++ src/Problems/Chapter16_Recursion/PalindromicDecompositions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Chapter16_Recursion
+{
+    public class PalindromicDecompositions
+    {
+        public IReadOnlyCollection<ICollection<string>> ComputeAllPalindromicDecompositions(string input)
+        {
+            if (input is null) throw new ArgumentNullException(nameof(input));
+            if (input.Length == 0) return Array.Empty<string[]>();
+            if (input.Length == 1) return new[] { new[] { input } };
+
+            var results = new List<List<string>>();
+
+            ComputeDecompositions(input, 0, input.Length - 1, new List<string>(), results);
+
+            return results;
+        }
+
+        private void ComputeDecompositions(string input, int startIndex, int endIndex, List<string> currentDecomposition, List<List<string>> results)
+        {
+            if (startIndex > endIndex)
+            {
+                results.Add(currentDecomposition.ToList());
+                return;
+            }
+
+            // ACCB -> {A,CC,B}, {A,C,C,B}
+            // brute - force - enumerate all decompositions
+            // check that every string inside is a palindrome
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                if (isPalindrome(input, startIndex, i))
+                {
+                    var length = i - startIndex + 1;
+                    var prefix = input.Substring(startIndex, length);
+                    currentDecomposition.Add(prefix);
+                    ComputeDecompositions(input, i + 1, endIndex, currentDecomposition, results);
+                    currentDecomposition.RemoveAt(currentDecomposition.Count - 1);
+                }
+            }
+        }
+
+        private bool isPalindrome(string input, int startIndex, int endIndex)
+        {
+            if (endIndex <= startIndex) return true;
+            for (int i = 0; i < (endIndex - startIndex + 1) / 2; i++)
+            {
+                if (input[startIndex + i] != input[endIndex - i]) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter16_Recursion/PalindromicDecompositionsTests.cs
@@ -0,0 +1,32 @@
+﻿using Problems.Chapter16_Recursion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Problems.Tests.Chapter16_Recursion
+{
+    public class PalindromicDecompositionsTests
+    {
+        PalindromicDecompositions sut = new();
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void ComputeAllPalindromicDecompositionsTest(string input, string[][] expected)
+        {
+            var actual = sut.ComputeAllPalindromicDecompositions(input);
+            TestUtils.AssertEquivalent(expected, actual);
+        }
+
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { "A", new[] { new[] { "A" } } };
+            yield return new object[] { "AB", new[] { new[] { "A", "B" } } };
+            yield return new object[] { "AA", new[] { new[] { "A", "A" }, new[] { "AA" } } };
+            yield return new object[] { "ABB", new[] { new[] { "A", "BB" }, new[] { "A", "B", "B" } } };
+        }
+    }
+}
