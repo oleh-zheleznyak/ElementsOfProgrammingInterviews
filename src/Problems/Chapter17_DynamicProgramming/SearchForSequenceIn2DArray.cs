10e394f3ab43ec13f7fe70bf66bb8a54b8422798
Oleh Zheleznyak <oleh.zheleznyak@gmail.com>
2022-04-24 16:39:17 +0200

    added cache

--- src/Problems/Chapter17_DynamicProgramming/SearchForSequenceIn2DArray.cs
+++ src/Problems/Chapter17_DynamicProgramming/SearchForSequenceIn2DArray.cs
@@ -9,12 +9,15 @@ public record struct Point(int row, int col)
        row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1);
 }
 
+public record struct PointWithLength(Point point, int length);
+
 public class SearchForSequenceIn2DArray
 {
     public bool MatrixContainsSequence(int[,] matrix, int[] sequence)
     {
         if (matrix is null) throw new ArgumentNullException(nameof(matrix));
         if (sequence is null) throw new ArgumentNullException(nameof(sequence));
+        var cache = new HashSet<PointWithLength>();
 
         for (var row = 0; row < matrix.GetLength(0); row++)
         {
@@ -22,7 +25,7 @@ public class SearchForSequenceIn2DArray
             {
                 var point = new Point(row, col);
                 var span = new ReadOnlySpan<int>(sequence, 0, sequence.Length);
-                var containsSequence = ContainsSequenceStartingAt(matrix, point, span);
+                var containsSequence = ContainsSequenceStartingAt(matrix, point, span, cache);
                 if (containsSequence) return true;
             }
         }
@@ -30,19 +33,25 @@ public class SearchForSequenceIn2DArray
     }
 
     // TODO: consider using Span instead of array + index
-    private bool ContainsSequenceStartingAt(int[,] matrix, Point start, ReadOnlySpan<int> sequence)
+    private bool ContainsSequenceStartingAt(int[,] matrix, Point start, ReadOnlySpan<int> sequence, HashSet<PointWithLength> cache)
     {
         if (sequence.IsEmpty) return true; // TODO; check - do we need this?
-        if (start.row >= matrix.GetLength(0)) return false;
-        if (start.col >= matrix.GetLength(1)) return false;
+        if (
+            start.row >= matrix.GetLength(0) ||
+            start.col >= matrix.GetLength(1)
+            ) return false;
         if (matrix[start.row, start.col] != sequence[0]) return false;
         if (sequence.Length == 1) return true;
 
-        // same as 
+        var pointWithLength = new PointWithLength(start, sequence.Length);
+        if (cache.Contains(pointWithLength)) return false;
+
         var adjacentCellsContainSeq = false;
         var nextSequence = sequence.Slice(1);
         foreach (var point in GetAdjacentPointsWithinMatrix(start, matrix))
-            adjacentCellsContainSeq = adjacentCellsContainSeq || ContainsSequenceStartingAt(matrix, point, nextSequence);
+            adjacentCellsContainSeq = adjacentCellsContainSeq || ContainsSequenceStartingAt(matrix, point, nextSequence, cache);
+        
+        if (adjacentCellsContainSeq == false) cache.Add(pointWithLength);
         return adjacentCellsContainSeq;
     }
 
