5c77683ca713f50efb06a548d8b22e2a7237e741
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2022-05-22 16:21:16 +0200

    brute force

---
+++ src/Problems/Chapter18_GreedyAlgorithms/The3SumProblem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Chapter18_GreedyAlgorithms
+{
+    public class The3SumProblem
+    {
+        public (int,int,int)? FindThreeEntriesThatSumUpTo(int sum, int[] array)
+        {
+            // brute force - 3 nested loops
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    for (int k = 0; k < array.Length; k++)
+                    {
+                        if (array[i]+array[j]+array[k]==sum) return (array[i] , array[j] , array[k]);
+                    }
+                }
+            }
+            return null;
+        } 
+    }
+}
