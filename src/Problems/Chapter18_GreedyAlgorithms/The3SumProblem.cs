15668760016e9399d89588421806fcc04fb74704
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2022-05-22 16:52:43 +0200

    re-implemented the algorithm using sorting

--- src/Problems/Chapter18_GreedyAlgorithms/The3SumProblem.cs
+++ src/Problems/Chapter18_GreedyAlgorithms/The3SumProblem.cs
@@ -10,18 +10,29 @@ namespace Problems.Chapter18_GreedyAlgorithms
     {
         public (int,int,int)? FindThreeEntriesThatSumUpTo(int sum, int[] array)
         {
-            // brute force - 3 nested loops
-            for (int i = 0; i < array.Length; i++)
+            if (array is null) throw new ArgumentNullException(nameof(array));
+            if (array.Length == 0) return null;
+
+            var sorted = array.OrderBy(x => x).ToArray();
+            return FindThreeEntitiesThatSumUpToSorted(sum, sorted);
+        }
+
+        private static (int, int, int)? FindThreeEntitiesThatSumUpToSorted(int sum, int[] array)
+        {
+            for (int k = 0; k < array.Length; k++)
             {
-                for (int j = 0; j < array.Length; j++)
+                var remainder = sum - array[k];
+                var i = 0;
+                var j = array.Length - 1;
+                while (i <= j)
                 {
-                    for (int k = 0; k < array.Length; k++)
-                    {
-                        if (array[i]+array[j]+array[k]==sum) return (array[i] , array[j] , array[k]);
-                    }
+                    var total = array[i] + array[j];
+                    if (total == remainder) return (array[k], array[i], array[j]);
+                    else if (total < remainder) i++;
+                    else j--;
                 }
             }
             return null;
-        } 
+        }
     }
 }
