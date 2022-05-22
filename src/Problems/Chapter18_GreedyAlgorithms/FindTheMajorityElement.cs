8b5a410b1debfd29b804dc597df398b3599a52be
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2022-05-22 19:35:41 +0200

    majority element via sorting

---
+++ src/Problems/Chapter18_GreedyAlgorithms/FindTheMajorityElement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Problems.Chapter18_GreedyAlgorithms;
+
+public class FindTheMajorityElement
+{
+    public string FindInArray(string[] array)
+    {
+        if (array is null) throw new ArgumentNullException(nameof(array));
+        if (array.Length == 0) throw new ArgumentException("Array is empty", nameof(array));
+        if (array.Length == 1) return array[0];
+
+        // option 1 - dictionary, O(n) time, O(n) space
+        // more than half are - majority
+        // option 2 - sort, loop, count (more than half) - O(nlgn) time, O(1) space if sort is in-place
+        Array.Sort(array);
+        var counter = 1;
+        var prev = array[0];
+        for (int i = 1; i < array.Length; i++)
+        {
+            var current = array[i];
+            if (current == prev) counter++;
+            else counter = 0;
+            
+            if (counter * 2 > array.Length) return current;
+            prev = current;
+        }
+        return null; // perhaps throw - pre-condition is broken
+    }
+}
---
+++ tests/Problems.Tests/Chapter18_GreedyAlgorithms/FindTheMajorityElementTests.cs
@@ -0,0 +1,18 @@
+﻿using Problems.Chapter18_GreedyAlgorithms;
+using Xunit;
+
+namespace Problems.Tests.Chapter18_GreedyAlgorithms;
+
+public class FindTheMajorityElementTests
+{
+    [Theory]
+    [InlineData(new string[] { "b", "a", "c", "a", "a", "b", "a", "a", "c", "a" }, "a")]
+    [InlineData(new string[] { "1", "2", "1" }, "1")]
+    [InlineData(new string[] { "1", "2", "1", "2", "1", "1" }, "1")]
+    public void FindInArrayTest(string[] array, string expected)
+    {
+        var sut = new FindTheMajorityElement();
+        var actual = sut.FindInArray(array);
+        Assert.Equal(expected, actual);
+    }
+}
