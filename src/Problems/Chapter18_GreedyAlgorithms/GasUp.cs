fdb3f221d109f469b8274b5f671c3be0434f2d6d
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2022-05-23 09:00:20 +0200

    brute force

---
+++ src/Problems/Chapter18_GreedyAlgorithms/GasUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Chapter18_GreedyAlgorithms
+{
+    public record City(string Name, uint GalonsOfFuel,uint DistanceToNextCityInMiles, City NextCity);
+    public class GasUp
+    {
+        // we can assume it exists
+        public City FindAmpleCity(City circularCityRoad, uint milesPerGalon)
+        {
+            if (circularCityRoad is null) throw new ArgumentNullException(nameof(circularCityRoad));
+
+            // brute force - try to start at every city + go full cycle - O(n^2)
+            // how to do this is linear time? - precompute? cache?
+
+            // select starting city
+            var start = circularCityRoad;
+            var current = circularCityRoad;
+            while (current.NextCity != start && current.NextCity!=null)
+            {
+                var mileage = CalculateMileageStartingAt(current, milesPerGalon);
+                if (mileage>0) return current;
+                current = current.NextCity;
+            }
+            throw new InvalidOperationException("no ample city exists: precondition broken");
+        }
+
+        private long CalculateMileageStartingAt(City city, uint milesPerGalon)
+        {
+            var start = city;
+            var current = city;
+            long milesBasedOnFuelInTank = 0;
+            while (current.NextCity != start && current.NextCity != null)
+            {
+                milesBasedOnFuelInTank += city.GalonsOfFuel * milesPerGalon;
+                milesBasedOnFuelInTank -= city.DistanceToNextCityInMiles;
+                if (milesBasedOnFuelInTank < 0) return milesBasedOnFuelInTank;
+
+                current = current.NextCity;
+            }
+            return milesBasedOnFuelInTank;
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter18_GreedyAlgorithms/GasUpTests.cs
@@ -0,0 +1,34 @@
+﻿using Problems.Chapter18_GreedyAlgorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Problems.Tests.Chapter18_GreedyAlgorithms;
+
+public class GasUpTests
+{
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void FindAmpleCityTest(City circularCityRoad, uint milesPerGallon, City expectedAmpleCity)
+    {
+        var sut = new GasUp();
+        var actualAmpleCity = sut.FindAmpleCity(circularCityRoad, milesPerGallon);
+        Assert.Equal(expectedAmpleCity, actualAmpleCity);
+    }
+
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return Basic();
+    }
+
+    private static object[] Basic()
+    {
+        var first = new City("A", 10 + 1, 100,
+            new City("B", 5, 50 + 1,
+                        new City("C", 1, 10 + 1, null)));
+        return new object[] { first, 10, first };
+    }
+}
