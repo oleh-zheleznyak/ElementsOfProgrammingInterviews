068bdd2a65dfb5d8bfd21fe014173f61577720b8
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2022-05-24 21:08:17 +0200

    brute force

---
+++ src/Problems/Chapter18_GreedyAlgorithms/MaxWaterTrappedByVerticalLines.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Chapter18_GreedyAlgorithms
+{
+    public class MaxWaterTrappedByVerticalLines
+    {
+        public (int,int) FindPairThatTrapsMaxWater(int[] array)
+        {
+            if (array is null) throw new ArgumentNullException(nameof(array));
+            if (array.Length<2) throw new ArgumentException("not enough elements", nameof(array));
+
+            // brute force - nested for loops, take every pair, compute volume - O(n^2) time, O(1) space
+            // sort - no O(nlgn)
+            // O(n)
+
+            var max = int.MinValue;
+            var pair = (max, max);
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    var width = j - i;
+                    var height = Math.Min(array[i], array[j]);
+                    var volume = width * height;
+                    if (volume>max)
+                    {
+                        max = volume;
+                        pair = (i,j);
+                    }
+                }
+            }
+            return pair;
+        }
+    }
+}
---
+++ tests/Problems.Tests/Chapter18_GreedyAlgorithms/MaxWaterTrappedByVerticalLinesTest.cs
@@ -0,0 +1,26 @@
+﻿using Problems.Chapter18_GreedyAlgorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Problems.Tests.Chapter18_GreedyAlgorithms;
+
+public class MaxWaterTrappedByVerticalLinesTest
+{
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void FindPairThatTrapsMaxWaterTest(int[] array, (int,int) expectedPair)
+    {
+        var sut = new MaxWaterTrappedByVerticalLines();
+        var actualPair = sut.FindPairThatTrapsMaxWater(array);
+        Assert.Equal(expectedPair, actualPair);
+    }
+
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return new object[] { new int[] { 1, 2, 1, 3, 4, 4, 5, 6, 2, 1, 3, 1, 3, 2, 1, 2, 4, 1 }, (4, 16) };
+    }
+}
