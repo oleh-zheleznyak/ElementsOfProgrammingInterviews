b470620366c8decadf35a759648f1d1889303901
oleh.zheleznyak <oleh.zheleznyak@gmail.com>
2022-05-30 20:26:26 +0200

    rewritten the algorithm from scratch, to remember better

--- src/Problems/Chapter18_GreedyAlgorithms/LargestRectangleUnderSkylineByTheBook.cs
+++ src/Problems/Chapter18_GreedyAlgorithms/LargestRectangleUnderSkylineByTheBook.cs
@@ -10,38 +10,56 @@ namespace Problems.Chapter18_GreedyAlgorithms
             if (heights is null) throw new ArgumentNullException(nameof(heights));
             if (heights.Length < 2) throw new ArgumentException("not enough elements", nameof(heights));
 
-            var pillarInices = new Stack<uint>();
-            var candidateRectangle = new Rectangle(0, 0, 0);
-
-            pillarInices.Push(0);
-            for (uint i = 1; i <= heights.Length; i++)
-            {
-                if (pillarInices.Count > 0 && i < heights.Length && heights[i] == heights[pillarInices.Peek()])
-                {
-                    pillarInices.Pop();
-                    pillarInices.Push(i);
-                }
-
-                while (pillarInices.Count > 0 && IsNewPillarOrReachEnd(heights, i, pillarInices.Peek()))
-                {
-                    var height = heights[pillarInices.Peek()];
-                    pillarInices.Pop();
-                    var start = pillarInices.Count > 0 ? pillarInices.Peek() + 1 : 0;
-                    var width = i - start;
-                    var area = height * width;
-                    if (area > candidateRectangle.Area)
-                        candidateRectangle = new Rectangle(start, i, height);
-                }
-                pillarInices.Push(i);
-            }
-            return candidateRectangle;
+            var pillarIndices = new Stack<uint>();
+            return ComputeLargest(heights, pillarIndices);
         }
 
-        private bool IsNewPillarOrReachEnd(uint[] heights, uint currentIndex, uint lastPillarIndex)
+        private Rectangle ComputeLargest(uint[] heights, Stack<uint> pillarIndices)
         {
-            return currentIndex < heights.Length ?
-                heights[currentIndex] < heights[lastPillarIndex]
-                : true;
+            var maxRectangle = new Rectangle(0, 0, 0);
+
+            pillarIndices.Push(0);
+            for (uint i = 1; i <= heights.Length; i++)
+            {
+                ReplaceDuplicateWithLatest(heights, pillarIndices, i);
+
+                while (IsNewPillarOrEndOfLine(heights, pillarIndices,i))
+                {
+                    var candidate = CandidateRectangle(heights, pillarIndices, i);
+
+                    if (candidate.Area > maxRectangle.Area)
+                        maxRectangle = candidate;
+                }
+                
+                pillarIndices.Push(i);
+            }
+            
+            return maxRectangle;
+        }
+
+        private Rectangle CandidateRectangle(uint[] heights, Stack<uint> pillarIndices, uint index)
+        {
+            var height = heights[pillarIndices.Peek()];
+            pillarIndices.Pop();
+            var start = pillarIndices.Count > 0 ? pillarIndices.Peek() + 1 : 0;
+            var end = index;
+            return new Rectangle(start, end, height);
+        }
+
+        private bool IsNewPillarOrEndOfLine(uint[] heights, Stack<uint> pillarIndices, uint index) =>
+            pillarIndices.Count>0 
+            &&
+            (index < heights.Length
+                ? heights[index] < heights[pillarIndices.Peek()]
+                : true);
+
+        private void ReplaceDuplicateWithLatest(uint[] heights, Stack<uint> pillarIndices, uint index)
+        {
+            if (index < heights.Length && pillarIndices.Count > 0 && heights[index] == heights[pillarIndices.Peek()])
+            {
+                pillarIndices.Pop();
+                pillarIndices.Push(index);
+            }
         }
     }
 }
